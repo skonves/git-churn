77e6881443b402411e1d0d3c4dac63825567fdfc
Steve Konves <stephen.konves@gmail.com>
2017-06-05 20:45:33 -0700

    test: include adds and dels

--- src/churn_test.go
+++ src/churn_test.go
@@ -35,13 +35,41 @@ func TestGetHunks(t *testing.T) {
 
 func TestParseHunk(t *testing.T) {
     // ARRANGE
-    hunkStr := "@@ -1,2 +3,5 @@ location\n text\n text"
+    hunkStr := "@@ -7,4 +1,5 @@ location\n nc1\n-del1\n-del2\n+add1\n nc2\n+add2\n nc3"
 
-    expectedHunk := Hunk{ Range{1, 2}, Range{3, 5}, " text\n text"}
+    expectedHunk := Hunk{ Range{7,4}, Range{1,5}, []int{8,9}, []int{2,4}, " nc1\n-del1\n-del2\n+add1\n nc2\n+add2\n nc3"}
 
     // ACT
     result := ParseHunk(hunkStr)
 
     // ASSERT
     assert.Equal(t, expectedHunk, result, "fail!")
+}
+
+func TestParseAdds(t *testing.T) {
+    // ARRANGE
+    start := 1
+    body  := " nc1\n-del1\n-del2\n+add1\n nc2\n+add2\n nc3"
+
+    expectedAdds := []int{2, 4}
+
+    // ACT
+    result := ParseAdds(start, body)
+
+    // ASSERT
+    assert.Equal(t, expectedAdds, result, "fail!")
+}
+
+func TestParseDels(t *testing.T) {
+    // ARRANGE
+    start := 7
+    body  := " nc1\n-del1\n-del2\n+add1\n nc2\n+add2\n nc3"
+
+    expectedDels := []int{8, 9}
+
+    // ACT
+    result := ParseDels(start, body)
+
+    // ASSERT
+    assert.Equal(t, expectedDels, result, "fail!")
 }
